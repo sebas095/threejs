461c00e2b5ae04d687c9738fe76bcf1a6f109aa9
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-02 17:21:48 -0500

    init project

---
+++ main.js
@@ -0,0 +1,19 @@
+(function () {
+  // Escena => Donde se colocan los objetos
+  // Cámara => Lo que se ve de la escena
+  // Render => Construir los gráficos
+
+  // Definimos la escena
+  let scene = new THREE.Scene();
+
+  // Definimos la cámara
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  let camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+
+  // Definir el render
+  let renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  renderer.render(scene, camera);
+})();
