ec6ffc750d588ba85e0ee65f06db93bd5a0348cc
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-02 17:39:14 -0500

    first objects and lights

--- main.js
+++ main.js
@@ -4,16 +4,33 @@
   // Render => Construir los gráficos
 
   // Definimos la escena
-  let scene = new THREE.Scene();
+  const scene = new THREE.Scene();
 
   // Definimos la cámara
   const aspectRatio = window.innerWidth / window.innerHeight;
-  let camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+  const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
 
   // Definir el render
-  let renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+
+  camera.position.z = 3;
+  camera.position.y = 1;
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const groundMaterial = new THREE.MeshPhongMaterial({
+    color: 0xffffff
+  });
+
+  const mesh = new THREE.Mesh(geometry, groundMaterial);
+  const pointLight = new THREE.PointLight(0xdfebff);
+  const ambientLight = new THREE.AmbientLight(0x404040);
+  pointLight.position.y = 30;
+
+  scene.add(mesh);
+  scene.add(ambientLight);
+  scene.add(pointLight);
 
   function loop() {
     requestAnimationFrame(loop);
